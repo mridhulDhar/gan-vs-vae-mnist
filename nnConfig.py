2d536ebd1df28724af7c98738e3dc05ee3b8eb0f
mridhulDhar <mdhar006@gmail.com>
2020-12-12 23:52:25 -0500

    ref

---
+++ nnConfig.py
@@ -0,0 +1,14 @@
+epochs = 200
+batches = 128
+
+adamLR = 0.002
+beta  = 0.5
+
+dimensionality = 100
+
+activation='sigmoid'
+loss='binary_crossentropy' 
+
+activationTanh = "tanh"
+activationRelu = "relu"
+padding = 'same'
